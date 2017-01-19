10ace0cf0b5ae4131964c013f3c246dd53004d14
Julia Franco <julia.franco1@marist.edu>
2017-01-19 00:10:50 -0500

    solution to execrise 1.4

---
+++ cmpt220franco/lab/1/PrintATable_1_4.java
@@ -0,0 +1,17 @@
+package firstlab;
+
+public class PrintATable_1_4 {
+	public static void main (String[] args) {
+		//print the following outputs:
+		//a a^2 a^3
+		//1 1 1
+		//2 4 8
+		//3 9 27
+		//4 16 64
+		System.out.println ("a     a^2    a^3");
+		System.out.println ("1     1      1  ");
+		System.out.println ("2     4      8  ");
+		System.out.println ("3     9      27 ");
+		System.out.println ("4     16     64 ");
+	}
+}
