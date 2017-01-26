a3e1180d275bba9313afeaa8d2b85dd99740bf7f
Julia Franco <julia.franco1@marist.edu>
2017-01-26 00:49:44 -0500

    Solution to exercise 4.9

---
+++ cmpt220franco/lab/2/Unicode4_9.java
@@ -0,0 +1,17 @@
+package two;
+
+import java.util.Scanner;
+
+public class Unicode4_9 {
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a character: ");
+		char chInput = input.next().charAt(0);
+		int ch = (int) chInput;
+
+		System.out.print("The unicode for the character " + chInput + " is " 
+		+ (int)ch);
+		
+	}
+
+}
