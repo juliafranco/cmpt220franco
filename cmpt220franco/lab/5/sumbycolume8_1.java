cea333ad7dd5eb3fe82f7b1ae702f652190bd18d
Julia Franco <julia.franco1@marist.edu>
2017-02-18 02:48:13 -0500

    solution to exercise 8.1

---
+++ cmpt220franco/lab/5/sumbycolume8_1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class sumbycolume8_1 {
+	public static void main (String[] args){
+		final int ROWS = 3;
+		final int COLUMNS = 4;
+		// Create a Scanner
+		Scanner input = new Scanner(System.in);
+		double[][] matrix = new double[ROWS][COLUMNS];
+		System.out.println("Enter a " + matrix.length + " by " + matrix[0].length + " matrix row by row ");
+		for (int i = 0; i < matrix.length; i++)
+			for (int j = 0; j < matrix[i].length; j++)
+				matrix[i][j] = input.nextInt();
+		for (int k = 0; k < matrix[0].length; k++){ //k is the column index
+			double total = sumColumn (matrix, k);
+			System.out.println("The sum of column " + (k + 1) + " is " + total);
+		}
+	}
+	public static double sumColumn (double [][] m, int columnIndex){
+		//find the total of each column.
+			double total = 0;
+			for ( int row = 0; row < m.length ; row++){
+				total += m[row][columnIndex];
+			}
+			return total;
+	}
+
+}
